31cc2bd6c453add09bfa589a30b8bd52f13def7d
Hanna-Ismahilova <hi.infoshare@gmail.com>
2019-07-17 09:13:17 +0200

    Project ControlCheck is added

---
+++ ControlCheck/Helpers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlCheck
+{
+    class Helpers
+    {
+
+        public List<Stanowiska> WygenerowanieDanychStanowiska()
+        {
+            var stanowiska = new List<Stanowiska>();
+
+            var stanowisko1 = new Stanowiska()
+            {
+                Id = 1,
+                NazwaStanowiska = "Junior",
+                ZarobkiOd = 1000,
+                ZarobkiDo = 4000
+            };
+            stanowiska.Add(stanowisko1);
+
+            var stanowisko2 = new Stanowiska()
+            {
+                Id = 2,
+                NazwaStanowiska = "Middle",
+                ZarobkiOd = 4001,
+                ZarobkiDo = 6000
+            };
+            stanowiska.Add(stanowisko2);
+
+            var stanowisko3 = new Stanowiska()
+            {
+                Id = 3,
+                NazwaStanowiska = "Senior",
+                ZarobkiOd = 6001,
+                ZarobkiDo = 9000
+            };
+            stanowiska.Add(stanowisko3);
+
+            return stanowiska;
+        }
+
+
+        public void WygenerowanieDanycPracownika()
+        {
+            var person = new Bogus.Person();
+
+            person.();
+        }
+
+
+    }
+}
---
+++ ControlCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ControlCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pracownik> pracownicy = new List<Pracownik>();
+
+            List<Stanowiska> stanowiska = new List<Stanowiska>();
+
+
+        }
+    }
+}
